08d7671af632f35ca940ee09f1959365a90c3811
YV <yvdevy@gmail.com>
2019-12-29 16:17:44 +0530

    Added a static and non static method

--- FullJavaTuts/src/com/YVCodes/FullJavaTuts/HiJava.java
+++ FullJavaTuts/src/com/YVCodes/FullJavaTuts/HiJava.java
@@ -2,8 +2,20 @@ package com.YVCodes.FullJavaTuts;
 
 public class HiJava {
 
+	public void nonStaticFn() {
+		System.out.println("This is a Non Static Method");
+		
+	}
+	public static void staticFn() {
+		System.out.println("I'm a static Method");
+		
+	}
 	public static void main(String[] args) {
+
 		System.out.println("Hi Java this is YV");
+		HiJava hijavaObj=new HiJava();
+		hijavaObj.nonStaticFn();
+		staticFn();
 
 	}
 
